e7bb98ef6dd3dbe36d94c08d3ef09d90deda1785
theGreyLord <krishnanj@mail.com>
2017-03-05 16:31:47 -0500

    Update Checkers1.java

--- Checkers1.java
+++ Checkers1.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
-public class Checkers1 {
-	static int[] checkerPositions;
-	static int[][] readyToPlayBoard;
+public class lightsout {
+
+	static String[] offPosArr;
+	static int[][] onPos;
+ 	static int[][] readyToPlayBoard;
 	static int x = 0;
 	static int y = 0;
 	
@@ -12,112 +14,136 @@ public class Checkers1 {
 		Scanner in = new Scanner(System.in);
 		String input = in.nextLine();
 		in.close();
-		checkerPositions = decodeInput(input);
+		onPos = decodeInput(input);
 		readyToPlayBoard = setupBoard();
-		playCheckers();
-	}
-
-	public static int[] decodeInput(String input) {
-
-		String[] arrIn = input.split(",");
-
-		checkerPositions = new int[arrIn.length];
-		int num = 0;
-		for (int i = 0; i < arrIn.length; i++) {
-			num = Integer.parseInt(arrIn[i]);
-			checkerPositions[i] = num - 1;
-		}
-
-		return checkerPositions;
-
-	}
-
-	public static int[][] setupBoard() {
-		readyToPlayBoard = new int[8][8];
-
-		// Loop to populate board with 0s
+		//playCheckers();
+		// Loop to print board
 		for (int i = 0; i <= 7; i++) {
 			for (int j = 0; j <= 7; j++) {
-				readyToPlayBoard[i][j] = 0;
-			}
-		}
-
-		// Loop to populate opponent positions
-		for (int h = 4; h < checkerPositions.length; h += 2) {
-			readyToPlayBoard[checkerPositions[h - 1]][checkerPositions[h]] = 1;
-			readyToPlayBoard[checkerPositions[0]][checkerPositions[1]] = 2;
-
-		}
-
-		return readyToPlayBoard;
-	}
-
-	public static boolean canCheckerMoveRight() {
-		boolean returnValue = false;
-		try {
-			if ((readyToPlayBoard[checkerPositions[0] + 1][checkerPositions[1] + 1] == 1)
-					&& (readyToPlayBoard[checkerPositions[0] + 2][checkerPositions[1] + 2] == 0))
-				returnValue = true;
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("I am a doofus because my array counter went out of bounds with this number: " + e.getMessage());
-			System.out.println("checkerPositions[0]= " + checkerPositions[0]);
-			System.out.println("checkerPositions[1]= " + checkerPositions[1]);
-		}
-		return returnValue;
-	}
-
-	public static boolean canCheckerMoveLeft() {
-		boolean returnValue = false;
-		try {
-			if ((readyToPlayBoard[checkerPositions[0] + 1][checkerPositions[1] - 1] == 1)
-					&& (readyToPlayBoard[checkerPositions[0] + 2][checkerPositions[1] - 2] == 0)) 
-				returnValue = true;
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("I am a doofus because my array counter went out of bounds with this number: " + e.getMessage());
-			System.out.println("checkerPositions[0]= " + checkerPositions[0]);
-			System.out.println("checkerPositions[1]= " + checkerPositions[1]);
-		}
-		
-		return returnValue;
-	}
-
-	public static void playCheckers() {
-		// Conditions
-		int count = 0;
-		boolean foundKing = false;
-		while (true) {
-			if ((checkerPositions[0] == 7)) {
-				foundKing = true;
-				break;
-			} else if (canCheckerMoveRight()) {
-				count += 1;
-				readyToPlayBoard[checkerPositions[0]][checkerPositions[1]] = 0;
-				readyToPlayBoard[checkerPositions[0] + 2][checkerPositions[1] + 2] = 2;
-				checkerPositions[0] += 2;
-				checkerPositions[1] += 2;
-			} else if (canCheckerMoveLeft()) {
-				count += 1;
-				readyToPlayBoard[checkerPositions[0]][checkerPositions[1]] = 0;
-				readyToPlayBoard[checkerPositions[0] + 2][checkerPositions[1] - 2] = 2;
-				checkerPositions[0] += 2;
-				checkerPositions[1] -= 2;
-			} else if ((checkerPositions[0] <= 0 || checkerPositions[1] <= 0)
-					|| (checkerPositions[0] <= 7 || checkerPositions[1] <= 7)) {
-				break;
-			}
-		}
-
-		if (foundKing)
-			System.out.println(count + ", KING");
-		else
-			System.out.println(count);
-
-		// Loop to print board
-		for (int a = 0; a <= 7; a++) {
-			for (int b = 0; b <= 7; b++) {
-				System.out.print(" " + readyToPlayBoard[a][b] + " ");
+				System.out.print(" " + readyToPlayBoard[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
+
+			public static int[][] decodeInput(String input) {
+				offPosArr = input.split(" ");
+				int firstNum = Integer.parseInt(offPosArr[0]);
+				for(int firstSet = 1; firstSet <= firstNum; firstSet++) {
+					int len = offPosArr[firstSet].length();
+					onPos = new int[offPosArr.length][len+1];
+					for(int fillArr = 0;fillArr<len;fillArr++) {
+						onPos[firstSet][fillArr] = Integer.parseInt(offPosArr[firstSet].substring(fillArr,fillArr+1));
+						System.out.print(" " + onPos[firstSet][fillArr] + " ");
+						//System.out.println(offPosArr[firstSet].substring(fillArr,fillArr+1));
+					}
+					System.out.println();
+					//System.out.println(offPosArr[firstSet].length());
+				}
+				//System.out.println("on pos "+onPos[1][0]);
+				int secondNum = Integer.parseInt(offPosArr[0])+1;
+				//System.out.println("secondNum"+secondNum);
+				for(int secondSet = secondNum +1; secondSet < offPosArr.length; secondSet++) {
+					int len2 = offPosArr[secondSet].length();
+					onPos = new int[offPosArr.length][len2+1];
+					for(int fillArr = 0;fillArr<len2;fillArr++) {
+						onPos[secondSet][fillArr] = Integer.parseInt(offPosArr[secondSet].substring(fillArr,fillArr+1));
+						System.out.print(" " + onPos[secondSet][fillArr] + " ");
+						//System.out.println(offPosArr[secondSet].substring(fillArr,fillArr+1));
+					}
+					System.out.println();
+					//System.out.println(offPosArr[secondSet].length());
+				}
+				
+				return onPos;
+			}
+			
+
+			
+			public static int[][] setupBoard() {
+				readyToPlayBoard = new int[8][8];
+
+				// Loop to populate board with 0s
+				for (int i = 0; i <= 7; i++) {
+					for (int j = 0; j <= 7; j++) {
+						readyToPlayBoard[i][j] = 0;
+					}
+				}
+
+				//Loop to populate on positions
+				int firstNum = Integer.parseInt(offPosArr[0]);
+				for(int firstSet = 1; firstSet <= firstNum; firstSet++) {
+					int len = offPosArr[firstSet].length();
+					for(int fillArr = 0;fillArr<len;fillArr++) {
+						readyToPlayBoard[onPos[firstSet][fillArr]][0] = 1;
+						System.out.println("1,0: "+onPos[1][0]);
+					}
+					//System.out.println(offPosArr[firstSet].length());
+				}
+				int secondNum = Integer.parseInt(offPosArr[0])+1;
+				for(int secondSet = secondNum +1; secondSet < offPosArr.length; secondSet++) {
+					int len2 = offPosArr[secondSet].length();
+					onPos = new int[offPosArr.length][len2+1];
+					for(int fillArr = 0;fillArr<len2;fillArr++) {
+						onPos[secondSet][fillArr] = Integer.parseInt(offPosArr[secondSet].substring(fillArr,fillArr+1));
+						//System.out.println(onPos[secondSet][fillArr]);
+						//System.out.println(offPosArr[secondSet].substring(fillArr,fillArr+1));
+					}
+					//System.out.println(offPosArr[secondSet].length());
+				}
+				return readyToPlayBoard;
+			}
 }
+			/*
+			public static boolean canCheckerMoveRight() {
+				boolean returnValue = false;
+				try {
+					if ((readyToPlayBoard[onPos[0] + 1][onPos[1] + 1] == 1)
+							&& (readyToPlayBoard[onPos[0] + 2][onPos[1] + 2] == 0))
+						returnValue = true;
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("I am a doofus because my array counter went out of bounds with this number: " + e.getMessage());
+					System.out.println("checkerPositions[0]= " + onPos[0]);
+					System.out.println("checkerPositions[1]= " + onPos[1]);
+				}
+				return returnValue;
+			}
+
+			public static boolean canCheckerMoveLeft() {
+				boolean returnValue = false;
+				try {
+					if ((readyToPlayBoard[onPos[0] + 1][onPos[1] - 1] == 1)
+							&& (readyToPlayBoard[onPos[0] + 2][onPos[1] - 2] == 0)) 
+						returnValue = true;
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("I am a doofus because my array counter went out of bounds with this number: " + e.getMessage());
+					System.out.println("checkerPositions[0]= " + onPos[0]);
+					System.out.println("checkerPositions[1]= " + onPos[1]);
+				}
+				
+				return returnValue;
+			}
+
+			public static void playCheckers() {
+				// Conditions
+				int count = 0;
+				boolean foundKing = false;
+				while (true) {
+					if ((onPos[0] == 7)) {
+						foundKing = true;
+						break;
+					} else if (canCheckerMoveRight()) {
+						count += 1;
+						readyToPlayBoard[onPos[0]][onPos[1]] = 0;
+						readyToPlayBoard[onPos[0] + 2][onPos[1] + 2] = 2;
+						onPos[0] += 2;
+						onPos[1] += 2;
+					} else if (canCheckerMoveLeft()) {
+						count += 1;
+						readyToPlayBoard[onPos[0]][onPos[1]] = 0;
+						readyToPlayBoard[onPos[0] + 2][onPos[1] - 2] = 2;
+						onPos[0] += 2;
+						onPos[1] -= 2;
+					} else if ((onPos[0] <= 0 || onPos[1] <= 0)
+							|| (onPos[0] <= 7 || onPos[1] <= 7)) {
+*/
